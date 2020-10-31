7abd1bb04be0988669a970c6ff240a64f944347b
Achmad Ichsan Fauzi <ichsan.fauzi@icloud.com>
2020-10-31 21:21:58 +0700

    Initial Commit

---
+++ Yo-Food-SwiftUI/Yo_Food_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yo_Food_SwiftUIApp.swift
+//  Yo-Food-SwiftUI
+//
+//  Created by Achmad Ichsan Fauzi on 31/10/20.
+//
+
+import SwiftUI
+
+@main
+struct Yo_Food_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
