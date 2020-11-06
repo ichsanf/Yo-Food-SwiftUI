8377a07d2ceabbe7952767fdd2a977b59d0c936f
Achmad Ichsan Fauzi <ichsan.fauzi@icloud.com>
2020-11-06 22:39:55 +0700

    Core Location Logic

--- Yo-Food-SwiftUI/View/Home.swift
+++ Yo-Food-SwiftUI/View/Home.swift
@@ -11,51 +11,64 @@ struct Home: View {
     @StateObject var HomeModel = HomeViewModel()
     
     var body: some View {
-        VStack(spacing: 10) {
-            HStack(spacing: 15) {
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                    Image(systemName: "line.horizontal.3")
-                        .font(.title)
+        ZStack {
+            VStack(spacing: 10) {
+                HStack(spacing: 15) {
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image(systemName: "line.horizontal.3")
+                            .font(.title)
+                            .foregroundColor(.pink)
+                    })
+                    
+                    Text(HomeModel.userLocation == nil ? "Locating..." : "Deliver To")
+                        .foregroundColor(.black)
+                    
+                    Text(HomeModel.userAddress)
+                        .font(.caption)
+                        .fontWeight(.heavy)
                         .foregroundColor(.pink)
-                })
+                    
+                    Spacer()
+                }
+                .padding([.horizontal, .top])
                 
-                Text("Deliver to")
-                    .foregroundColor(.black)
+                Divider()
                 
-                Text("Apple")
-                    .font(.caption)
-                    .fontWeight(.heavy)
-                    .foregroundColor(.pink)
+                HStack(spacing: 15) {
+                    TextField("Search", text: $HomeModel.search)
+                    
+                    if HomeModel.search != "" {
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                            Image(systemName: "magnifyingglass")
+                                .font(.title2)
+                                .foregroundColor(.gray)
+                        })
+                        .animation(.easeIn)
+                    }
+                }
+                .padding(.horizontal)
+                .padding(.top, 10)
+                
+                Divider()
                 
                 Spacer()
             }
-            .padding([.horizontal, .top])
             
-            Divider()
-            
-            HStack(spacing: 15) {
-                TextField("Search", text: $HomeModel.search)
-                
-                if HomeModel.search != "" {
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        Image(systemName: "magnifyingglass")
-                            .font(.title2)
-                            .foregroundColor(.gray)
-                    })
-                    .animation(.easeIn)
-                }
+            // Non-Closeable Alert if Permission Denied...
+            if HomeModel.noLocation {
+                Text("Please Enable Location Access in Settings for Further Use")
+                    .foregroundColor(.black)
+                    .frame(width: UIScreen.main.bounds.width - 100, height: 120)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.black.opacity(0.3).ignoresSafeArea())
             }
-            .padding(.horizontal)
-            .padding(.top, 10)
-            
-            Divider()
-            
-            Spacer()
         }
         .onAppear(perform: {
             // Calling location delegate...
             HomeModel.LocationManager.delegate = HomeModel
-            HomeModel.LocationManager.requestWhenInUseAuthorization()
+            
         })
     }
 }
--- Yo-Food-SwiftUI/ViewModel/HomeViewModel.swift
+++ Yo-Food-SwiftUI/ViewModel/HomeViewModel.swift
@@ -13,21 +13,56 @@ class HomeViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
     @Published var LocationManager = CLLocationManager()
     @Published var search = ""
     
+    // Location Details Variables...
+    @Published var userLocation: CLLocation!
+    @Published var userAddress = ""
+    @Published var noLocation = false
+    
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         
         //Checking Location Access...
         switch manager.authorizationStatus {
         case .authorizedWhenInUse:
             print("authorized")
+            manager.requestLocation()
+            self.noLocation = false
         case .denied:
             print("denied")
+            self.noLocation = true
         default:
             print("unknown")
+            self.noLocation = false
+            LocationManager.requestWhenInUseAuthorization()
         }
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         
         print(error.localizedDescription)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        // Reading User Location and Extracting Details...
+        self.userLocation = locations.last
+        self.extractLocation()
+    }
+    
+    func extractLocation() {
+        CLGeocoder().reverseGeocodeLocation(self.userLocation) { (res, err) in
+            guard let safeData = res
+            else {
+                return
+            }
+            
+            var address = ""
+            
+            // Getting Area and Local names
+            address += safeData.first?.name ?? ""
+            address += ", "
+            address += safeData.first?.locality ?? ""
+            
+            self.userAddress = address
+        }
     }
 }
